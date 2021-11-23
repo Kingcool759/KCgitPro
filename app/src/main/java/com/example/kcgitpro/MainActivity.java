6269e6ff1846867fe05af8b18be1956b7336fb47
zhaojianwei03 <zhaojianwei03@baidu.com>
2021-11-23 11:46:15 +0800

    commit-1

--- app/src/main/java/com/example/kcgitpro/MainActivity.java
+++ app/src/main/java/com/example/kcgitpro/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.kcgitpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(App.getContext(),"hhahha~",Toast.LENGTH_LONG).show();
+        // dev_commit-1
+        Log.i(TAG, "commit-1");
     }
 }
